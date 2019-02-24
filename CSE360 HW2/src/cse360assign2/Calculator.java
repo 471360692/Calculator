0406e7d1b1884c68019ee97901fd6c9e2fd6e9c9
Kekang Shu <Kekang Shu@DESKTOP-PETT6VU>
2019-02-23 22:18:58 -0700

    First commit.

---
+++ CSE360 HW2/src/cse360assign2/Calculator.java
@@ -0,0 +1,65 @@
+/**
+ * @author Kekang Shu 
+ * ClassID: 548
+ * Assignment Number: 1
+ * Description: This is the second assignment for CSE360 class.
+ */
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * 
+	 * This method will return the total number.
+	 * 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * 
+	 * @param value The integer to be added.
+	 * This method will add the parameter to the total variable.
+	 * 
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * 
+	 * @param value The integer to be subtracted.
+	 * This method will subtract the parameter from the total variable.
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * 
+	 * @param value the integer to be multiplied with.
+	 * This method will multiply the total by the parameter.
+	 */
+	public void multiply (int value) {
+		
+	}
+	/**
+	 * 
+	 * @param value the integer to be divide by.
+	 * This method will divide the total by the parameter.
+	 */
+	public void divide (int value) {
+		
+	}
+	/**
+	 * 
+	 * This method will return history.
+	 * 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
